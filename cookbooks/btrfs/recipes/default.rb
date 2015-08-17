2e54219df77de717fd9319f50ae5d4396ae8cc2c
Isak-Pao <isak-pao@capcom.com>
2015-08-17 12:28:20 +0800

    docker cookbook update

--- cookbooks/btrfs/recipes/default.rb
+++ cookbooks/btrfs/recipes/default.rb
@@ -1,20 +1,13 @@
-if %w{rhel}.include?(node['platform_family']) and 7 > node['platform_version'].to_i
-	package 'btrfs-progs' do
-		action :install
-	end
+package 'btrfs-progs' do
+	action :install
+end
 
-	bash 'add-kernel-module-loading-01' do
-		code 'echo modprobe btrfs >> /etc/rc.modules'
-  	not_if "lsmod | grep btrfs"
-	end
+bash 'add-kernel-module-loading' do
+	code 'echo modprobe btrfs >> /etc/rc.modules && chmod +x /etc/rc.modules'
+  not_if "lsmod | grep btrfs"
+end
 
-	bash 'add-kernel-module-loading-02' do
-		code 'chmod +x /etc/rc.modules'
-  	not_if "test -x /etc/rc.modules"
-	end
-
-	bash 'load-kernel-module' do
-		code 'modprobe btrfs'
-		not_if "lsmod | grep btrfs"
-	end
+bash 'load-kernel-module' do
+	code 'modprobe btrfs'
+	not_if "lsmod | grep btrfs"
 end
