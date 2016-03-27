519c29409303dc5432330467d42e73224fb1e3f3
windperson <windperson@gmail.com>
2016-03-27 10:05:55 +0800

    fix bash command

--- cookbooks/centos7nic-patch/recipes/default.rb
+++ cookbooks/centos7nic-patch/recipes/default.rb
@@ -2,7 +2,6 @@ if %w{rhel}.include?(node['platform_family']) and 7 <= node['platform_version'].
   # CentOS box stupid bug:
   # https://github.com/mitchellh/vagrant/issues/2614#issuecomment-108050799
   bash 'make-fake-nic-detect-rule' do
-    code "rm -f /etc/udev/rules.d/70-persistent-net.rules
-    && ln -sf /dev/null /etc/udev/rules.d/70-persistent-net.rules"
+    code "rm -f /etc/udev/rules.d/70-persistent-net.rules && ln -sf /dev/null /etc/udev/rules.d/70-persistent-net.rules"
   end
 end
