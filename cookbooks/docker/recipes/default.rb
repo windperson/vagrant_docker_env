8d7515b0fda5f4c51c57163d2fbe9cd429418931
windperson <windperson@gmail.com>
2016-04-04 14:39:02 +0800

    fix detect Redhat family OS error

--- cookbooks/docker/recipes/default.rb
+++ cookbooks/docker/recipes/default.rb
@@ -2,12 +2,12 @@ OS_name=node['platform']
 OS_family=node['platform_family']
 OS_ver=node['platform_version'].to_f
 log 'print OS info' do
-  message "OS=#{OS_name}, OS_family=#{OS_family} ver=#{OS_ver}"
+  message "OS_name=#{OS_name}, OS_family=#{OS_family} ver=#{OS_ver}"
   level :info
 end
 
-IsCentOS7orAbove = ( %w{rhel}.include?(OS_name) and %w{centos}.include?(OS_family) and 7 <= OS_ver )
-IsCentOS6 = ( %w{rhel}.include?(OS_name) and  %w{centos}.include?(OS_family) and 7 > OS_ver )
+IsCentOS7orAbove = ( %w{centos}.include?(OS_name) and %w{rhel}.include?(OS_family) and 7 <= OS_ver )
+IsCentOS6 = ( %w{centos}.include?(OS_name) and  %w{rhel}.include?(OS_family) and 7 > OS_ver )
 IsUbuntu = ( %w{ubuntu}.include?(OS_name) and %w{debian}.include?(OS_family) )
 
 use_bundle_installer = false
@@ -26,7 +26,7 @@ end
 
 package ['device-mapper', 'device-mapper-event-libs'] do
   action :upgrade
-  only_if { IsCentOS6 or IsCentOS7orAbove }
+  only_if { (IsCentOS6 or IsCentOS7orAbove) }
 end
 
 if use_bundle_installer
